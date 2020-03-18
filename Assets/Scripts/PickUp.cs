9461518a85c32f15fb44dde9251b5e5f6dabf20d
rod martin <rod@araknia.com>
2020-03-18 12:00:34 +0000

    chase the pick up

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+    Vector3 move = Vector3.zero;
+    public float speed = 1f;
+    void Update()
+    {
+        transform.Translate(move * Time.deltaTime);
+    }
     private void OnTriggerEnter(Collider other)
     {
         GameManagerSingleton.Instance.Score += 1;
         Vector3 newPos = transform.position;
-        newPos.x += Random.RandomRange(-5f, 5f);
-        newPos.z += Random.RandomRange(-5f, 5f);
+        newPos.x += Random.RandomRange(-6f, 6f);
+        newPos.z += Random.RandomRange(-6f, 6f);
         transform.position = newPos;
+        move.x = Random.RandomRange(-speed, speed);
+        move.z = Random.RandomRange(-speed, speed);
     }
 }
